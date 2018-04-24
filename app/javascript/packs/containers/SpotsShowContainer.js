9c51b733187a06ec5beda622e23b67875efff1fb
ChoiChips <christianchoi2@gmail.com>
2018-04-24 14:26:55 -0400

    completed show container

--- app/javascript/packs/containers/SpotsShowContainer.js
+++ app/javascript/packs/containers/SpotsShowContainer.js
@@ -4,12 +4,45 @@ import { Link } from 'react-router';
 class SpotsShowContainer extends Component {
   constructor(props){
     super(props)
-    this.state = { spots: [] }
+    this.state = {
+      spot:  {}
+    }
   }
+
+  componentDidMount() {
+    let spotId = this.props.params.id
+    fetch(`/api/v1/spots/${spotId}`, {
+      credentials: 'same-origin'
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+    .then(response => response.json())
+    .then(spot => {
+      this.setState ({ spot: spot})
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+    })
+  }
+
   render() {
-    return (
-      <div>spots show container</div>
-    );
+    return(
+      <SpotShow
+        id={this.state.spot.id}
+        name={this.state.spot.name}
+        artist={this.state.spot.artist}
+        rating={this.state.spot.rating}
+        location={this.state.spot.location}
+        access={this.state.spot.access}
+        aesthetic={this.state.spot.aesthetic}
+      />
+    )
   }
 }
 

19f2df7f0e67ec5306396b00452519fb785458cf
ChoiChips <christianchoi2@gmail.com>
2018-04-25 16:49:56 -0400

    Updated SpotsShowContainer

--- app/javascript/packs/containers/SpotsShowContainer.js
+++ app/javascript/packs/containers/SpotsShowContainer.js
@@ -5,7 +5,8 @@ class SpotsShowContainer extends Component {
   constructor(props){
     super(props)
     this.state = {
-      spot:  {}
+      spot:  {},
+      reviews: {}
     }
   }
 
@@ -28,20 +29,46 @@ class SpotsShowContainer extends Component {
       this.setState ({ spot: spot})
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`))
+
+    fetch(`/api/v1/spots/${spotId}/reviews`, {
+      credentials: 'same-origin'
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+    .then(response => response.json())
+    .then(reviews => {
+      this.setState ({ reviews: reviews})
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
 
   render() {
     return(
-      <SpotShow
-        id={this.state.spot.id}
-        name={this.state.spot.name}
-        artist={this.state.spot.artist}
-        description={this.state.spot.description}
-        rating={this.state.spot.rating}
-        location={this.state.spot.location}
-        access={this.state.spot.access}
-        aesthetic={this.state.spot.aesthetic}
-      />
+      <div className="row spot-container">
+        <SpotShow
+          key={this.state.spot.id}
+          id={this.state.spot.id}
+          name={this.state.spot.name}
+          artist={this.state.spot.artist}
+          description={this.state.spot.description}
+          rating={this.state.spot.rating}
+          location={this.state.spot.location}
+          access={this.state.spot.access}
+          aesthetic={this.state.spot.aesthetic}
+        />
+        <ReviewsShow
+          key={this.state.spot.id}
+
+        />
+      </div>
+
     )
   }
 }

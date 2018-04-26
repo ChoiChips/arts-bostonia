0002e9dca95d914df935e55516fdc727a270247a
ChoiChips <christianchoi2@gmail.com>
2018-04-26 10:19:01 -0400

    Got the SpotsShowContainer and SpotShow to work. Did some styling.

--- app/javascript/packs/components/SpotShow.js
+++ app/javascript/packs/components/SpotShow.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const SpotShow = (props) => {
   return(
-    <div className="spot-show">
+    <div className="show column small-12 medium-12 large-6">
       <h2>{props.name}</h2>
       <p>{props.description}</p>
       <p>{props.location}</p>
       <p>{props.artist}</p>
-      <p>{props.rating}</p>
-      <p>{props.access}</p>
-      <p>{props.aesthetic}</p>
+      <div><img src={props.photo}/></div>
     </div>
   )
 }
+
+
+export default SpotShow;
--- app/javascript/packs/containers/SpotsShowContainer.js
+++ app/javascript/packs/containers/SpotsShowContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import SpotShow from "../components/SpotShow"
+// import { Link } from 'react-router';
 
 class SpotsShowContainer extends Component {
   constructor(props){
@@ -26,28 +27,30 @@ class SpotsShowContainer extends Component {
       })
     .then(response => response.json())
     .then(spot => {
-      this.setState ({ spot: spot})
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`))
-
-    fetch(`/api/v1/spots/${spotId}/reviews`, {
-      credentials: 'same-origin'
-    })
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`,
-          error = new Error(errorMessage);
-          throw(error);
-        }
-      })
-    .then(response => response.json())
-    .then(reviews => {
-      this.setState ({ reviews: reviews})
+      this.setState ({ spot: spot.spot})
+      console.log(this.state.spot.photo)
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
+
+  //   fetch(`/api/v1/spots/${spotId}/reviews`, {
+  //     credentials: 'same-origin'
+  //   })
+  //   .then(response => {
+  //     if (response.ok) {
+  //       return response;
+  //     } else {
+  //       let errorMessage = `${response.status} (${response.statusText})`,
+  //         error = new Error(errorMessage);
+  //         throw(error);
+  //       }
+  //     })
+  //   .then(response => response.json())
+  //   .then(reviews => {
+  //     this.setState ({ reviews: reviews})
+  //   })
+  //   .catch(error => console.error(`Error in fetch: ${error.message}`))
+  // }
 
   render() {
     return(
@@ -58,17 +61,10 @@ class SpotsShowContainer extends Component {
           name={this.state.spot.name}
           artist={this.state.spot.artist}
           description={this.state.spot.description}
-          rating={this.state.spot.rating}
           location={this.state.spot.location}
-          access={this.state.spot.access}
-          aesthetic={this.state.spot.aesthetic}
-        />
-        <ReviewsShow
-          key={this.state.spot.id}
-
+          photo={this.state.spot.photo}
         />
       </div>
-
     )
   }
 }

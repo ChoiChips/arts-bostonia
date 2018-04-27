1e8df330f212d29e8517d768eda3c9318c17836e
ChoiChips <christianchoi2@gmail.com>
2018-04-27 16:39:34 -0400

    - Mild changes to user.rb and spots_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
 
 
    has_many :reviews
    has_many :spots
    mount_uploader :avatar, AvatarUploader
-=======
-  mount_uploader :avatar, AvatarUploader
->>>>>>> 65beb28ab14ce63bc8a957df8f58ae27f37cf284
 end

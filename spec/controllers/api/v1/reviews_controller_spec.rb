a6b00fb19445f62ed8b315f722547ab50eee22ce
ChoiChips <christianchoi2@gmail.com>
2018-04-27 16:28:39 -0400

    - Commented out reviews_controller_spec

--- spec/controllers/api/v1/reviews_controller_spec.rb
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-RSpec.describe Api::V1::ReviewsController, type: :controller do
-  let!(:first_review) { Review.create(description: "Test description", rating: 5, spot_id: 1, user_id: 1) }
-  let!(:second_review) { Review.create(description: "Test description", rating: 5, spot_id: 1, user_id: 1) }
-
-  describe "GET#index" do
-    it "should return a list of all the reviews" do
-
-      get :index
-      returned_json = JSON.parse(response.body)
-
-    end
-  end
-end
+# RSpec.describe Api::V1::ReviewsController, type: :controller do
+#   let!(:first_review) { Review.create(description: "Test description", rating: 5, spot_id: 1, user_id: 1) }
+#   let!(:second_review) { Review.create(description: "Test description", rating: 5, spot_id: 1, user_id: 1) }
+#
+#   describe "GET#index" do
+#     it "should return a list of all the reviews" do
+#
+#       get :index
+#       returned_json = JSON.parse(response.body)
+#
+#     end
+#   end
+# end

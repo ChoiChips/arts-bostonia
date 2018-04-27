e737c0bfe53d324d909aec1483f44a75995b12d5
Greg Garabedian <ggarabedian09@gmail.com>
2018-04-27 18:49:20 -0400

    chore: removed extraneous test files

--- spec/javascript/containers/spots_show_spec.js
+++
@@ -1,9 +0,0 @@
-import SpotIndexContainer from '../../app/javascript/packs/container'
-
-describe('SpotsIndexContainer', () => {
-  let wrapper;
-  beforeEach() => {
-    spyOn(SpotIndexContainer)
-
-  }
-} )

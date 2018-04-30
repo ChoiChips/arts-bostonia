4324f2d18bc63c0948324f5c6a407623ca319cac
Greg Garabedian <ggarabedian09@gmail.com>
2018-04-30 14:23:42 -0400

    adding foundation classes to forms

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,10 +2,7 @@ class SpotsController < ApplicationController
 
   before_action :authorize_user, except: [:index,:show]
 
-
   def index
-    # @last = Spot.last
-    # binding.pry
   end
 
   def show
@@ -15,21 +12,7 @@ class SpotsController < ApplicationController
     @spot = Spot.new
   end
   def create
-    # how do we specify the full AWS path before storing it in the DB:
-    # img_filename = spot_params[:photo].original_filename
-    # aws_url = "https://s3.amazonaws.com/arts-bostonia/arts-bostnia-development/"
-    # spot_params[:photo] = aws_url + img_filename
-
-
     @spot = Spot.new(spot_params)
-    # @spot.photo = aws_url + img_filename
-    # binding.pry
-    # img_directory = "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/"
-
-    # if Rails.env.development?
-    #   @spot.photo = aws_url + img_directory + "#{@spot.photo.filename}"
-    #   binding.pry
-    # end
 
     @spot.user = current_user
     if @spot.save
@@ -37,7 +20,6 @@ class SpotsController < ApplicationController
       @spot.save
       redirect_to @spot
     else
-      # refactor: add this directly to view template with logic
       @errors = @spot.errors.full_messages
       render :new
     end
@@ -55,10 +37,8 @@ class SpotsController < ApplicationController
       @spot = @spot.update(spot_params)
       redirect_to spot_path
     else
-      # flash[:fail] = 'Spot failed update'
       @errors = @spot.errors.full_messages
       render :edit
-
     end
 
   end

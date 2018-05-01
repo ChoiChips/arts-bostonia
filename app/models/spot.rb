cb7393d1bf26f1dbd724430bde6965d9229113b5
Daniel Davis <danis1911@gmail.com>
2018-05-01 16:03:41 -0400

    more tiny tweaks

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,18 +1,11 @@
 class Spot < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :user
-  has_many :reviews
 
   mount_uploader :photo, PhotoUploader
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
   validates :description, presence: true
-=======
-  validates :name, presence: true, uniqueness: true
-  validates :location, presence: true
-  validates :description, presence: true
 
   has_many :reviews
   belongs_to :user
->>>>>>> master
+
 end

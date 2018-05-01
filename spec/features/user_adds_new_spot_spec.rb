eb597e07d6080bec9e18954cd3c5094fe8ab7567
Greg Garabedian <ggarabedian09@gmail.com>
2018-05-01 15:40:43 -0400

    fixing capybara test

--- spec/features/user_adds_new_spot_spec.rb
+++ spec/features/user_adds_new_spot_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
-
-# As an authenticated user
-# I want to create a new installation "spot"
-# So that I can share interesting art that I found
-
-# Acceptance Criteria
-# [] I should see an “Add Art Spot” link in the top bar, visible from every page.’
-# [] I should see an “Add New Art Spot” callout link in the body of the page.
-# [] I should add a new art spot and save it to the database.
-
 feature 'authenticated user adds new spot' do
   before(:each) do
     user = FactoryBot.create(:user)
+
     visit new_user_session_path
 
     fill_in 'Email', with: user.email
@@ -25,6 +16,13 @@ feature 'authenticated user adds new spot' do
     visit new_spot_path
 
     expect(page).to have_content('Add New Art Spot')
+    fill_in "Name", with: "Cool new art spot"
+    fill_in "Description", with: "Lorem ipsum dolum sit amet"
+    fill_in "Location", with: "Boston, MA"
+
+    click_button "Add Spot"
+
+    expect(page).to have_content("Spot added successfully")
 
   end
 end

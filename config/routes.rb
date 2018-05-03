dfc01d326e802d3091967a5fc77187b17096229d
Daniel Davis <danis1911@gmail.com>
2018-05-03 10:48:29 -0400

    minor routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,9 @@ Rails.application.routes.draw do
         resources :reviews, only: [:index, :create]
       end
 
-<<<<<<< HEAD
-      resources :users, only: [:index]
-
-      # how do we nest; /users/spots, /users/shmugana
-      # resources :users, only: [:index] do
-      #   resources :spots, only: [:index]
-      # end
-=======
       resources :users, only: [:index, :show] do
         resources :spots, only: [:index]
       end
->>>>>>> bf60973a8758877e61f50dfe25c30eaa2881f4f1
 
       resources :reviews, only: [:index, :show, :create]
     end

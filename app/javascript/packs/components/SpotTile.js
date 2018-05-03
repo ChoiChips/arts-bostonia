897bf703f382cda81dcecbba272e8bb841e92158
ChoiChips <christianchoi2@gmail.com>
2018-05-03 14:04:19 -0400

    - Updated Spot Tile and Spot Serializer

--- app/javascript/packs/components/SpotTile.js
+++ app/javascript/packs/components/SpotTile.js
@@ -5,15 +5,24 @@ const SpotTile = (props) => {
 
   let photo = props.photo.url || ''
   let name = props.name.toUpperCase()
+  let description = ''
+  if (props.description.length > 40) {
+    description = `${props.description.substring(0, 40)}...`
+  } else {
+    description = props.description
+  }
 
   return (
     <div className="columns small-12 medium-6 large-4 end">
       <Link className="spot-tile" to={`/spots/${props.id}`}>
           <div className="title"> {name} </div>
           <img className="photo" src={`${photo}`}/>
-          <div>{props.description}</div>
-          <div>{props.location}</div>
-          <div>{props.artist}</div>
+          <div>
+            Description: {description}
+          </div>
+          <div>
+            Location: {props.location}
+          </div>
       </Link>
     </div>
   );
--- app/serializers/spot_serializer.rb
+++ app/serializers/spot_serializer.rb
@@ -1,3 +1,3 @@
 class SpotSerializer < ActiveModel::Serializer
-  attributes :id, :name, :photo, :artist, :description
+  attributes :id, :name, :photo, :artist, :description, :location
 end
